541e9d1e5d0b8da223cbf082c3c314839f7774d0
Sonu Jha <lokesh.ys@entosstech.com>
2019-01-29 14:12:54 +0530

    new profram made

---
+++ core java/src/coreJava/Example4.java
@@ -0,0 +1,12 @@
+package coreJava;
+
+public class Example4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("learning git");
+
+	}
+
+}
