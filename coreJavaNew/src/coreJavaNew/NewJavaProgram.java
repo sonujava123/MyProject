1ff2bcd704ecb997bc017e4f76a41c37c0c50954
Lokesh Yadav <lokesh.ys@entosstech.com>
2019-02-12 19:25:22 +0530

    new core java

---
+++ coreJavaNew/src/coreJavaNew/NewJavaProgram.java
@@ -0,0 +1,11 @@
+package coreJavaNew;
+
+public class NewJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("new folder made");
+	}
+
+}
